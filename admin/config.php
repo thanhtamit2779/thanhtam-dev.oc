a5aa91ccc1d4e5516fb2ccf8dff34afa5bb70720
DESKTOP-FHPRAEF\thanh <thanhtam.it2779@gmail.com>
2017-08-30 19:21:17 +0700

    Opencart 3.0.2.0
    Opencart 3.0.2.0 phiên bản gốc

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/thanhtam-dev.oc/admin/');
+define('HTTP_CATALOG', 'http://localhost/thanhtam-dev.oc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/thanhtam-dev.oc/admin/');
+define('HTTPS_CATALOG', 'http://localhost/thanhtam-dev.oc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/thanhtam-dev.oc/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/thanhtam-dev.oc/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/thanhtam-dev.oc/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/thanhtam-dev.oc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '_thanhtam-dev.oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
