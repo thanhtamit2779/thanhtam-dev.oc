3126449ca90ce6b79e6e8941fea16fa5222a9772
Unknown <thanhtam.it2779@gmail.com>
2017-10-18 15:03:17 +0700

    [DEVELOPER] - FIX LẠI GIAO DIỆN CAROUSEL

--- catalog/controller/extension/module/carousel.php
+++ catalog/controller/extension/module/carousel.php
@@ -6,9 +6,9 @@ class ControllerExtensionModuleCarousel extends Controller {
 		$this->load->model('design/banner');
 		$this->load->model('tool/image');
 		
-		$this->document->addStyle('catalog/view/javascript/jquery/swiper/css/swiper.min.css');
-		$this->document->addStyle('catalog/view/javascript/jquery/swiper/css/opencart.css');
-		$this->document->addScript('catalog/view/javascript/jquery/swiper/js/swiper.jquery.js', 'footer');
+		// $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/swiper.min.css');
+		// $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/opencart.css');
+		// $this->document->addScript('catalog/view/javascript/jquery/swiper/js/swiper.jquery.js', 'footer');
 
 		$data['banners'] = array();
 
